d310322049e765eb30df1435409dd02f1f048b45
Plopmenz <plopmenz@gmail.com>
2026-05-05 21:21:43 +0200

    handle CNAMES

--- directdns.go
+++ directdns.go
@@ -1,115 +1,218 @@
 package directdns
 
 import (
-	"context"
-	"net"
-	"strings"
-	"time"
+    "context"
+    "net"
+    "strings"
+    "time"
 
-	"github.com/coredns/coredns/plugin"
-	"github.com/coredns/coredns/request"
+    "github.com/coredns/coredns/plugin"
+    "github.com/coredns/coredns/plugin/pkg/log"
+    "github.com/coredns/coredns/request"
 
-	"github.com/miekg/dns"
+    "github.com/miekg/dns"
 )
 
 type DirectDNS struct {
-	Next  plugin.Handler
-	Zones []string
+    Next  plugin.Handler
+    Zones []string
+}
+
+type captureWriter struct {
+    dns.ResponseWriter
+    msg *dns.Msg
+}
+
+func (cw *captureWriter) WriteMsg(msg *dns.Msg) error {
+    cw.msg = msg.Copy()
+    return nil
 }
 
 var _ plugin.Handler = &DirectDNS{}
 
 func (directdns *DirectDNS) Name() string { return "directdns" }
+
 func (directdns *DirectDNS) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-	state := request.Request{W: w, Req: r}
+    state := request.Request{W: w, Req: r}
+    queryName := state.Name()
+    log.Infof("[directdns] ServeDNS: query=%s, zones=%v", queryName, directdns.Zones)
 
-	queryName := state.Name()
+    zone := directdns.matchZone(queryName)
+    if zone != "" {
+        log.Infof("[directdns] direct match: query=%s, zone=%s", queryName, zone)
+        return directdns.handleDirectQuery(ctx, r, w, zone, queryName)
+    }
 
-	zone := directdns.matchZone(queryName)
-	if zone == "" {
-		return plugin.NextOrFailure(directdns.Name(), directdns.Next, ctx, w, r)
-	}
-
-	subdomain := trimZone(queryName, zone)
-	if subdomain == "" {
-		return plugin.NextOrFailure(directdns.Name(), directdns.Next, ctx, w, r)
-	}
-
-	ipv6 := decodeIPv6(subdomain)
-	if ipv6 == "" {
-		return plugin.NextOrFailure(directdns.Name(), directdns.Next, ctx, w, r)
-	}
-
-	resp, err := forwardToNode(r, ipv6)
-	if err != nil || resp == nil {
-		return plugin.NextOrFailure(directdns.Name(), directdns.Next, ctx, w, r)
-	}
-
-	resp.SetReply(r)
-	resp.Authoritative = true
-
-	_ = w.WriteMsg(resp)
-	return dns.RcodeSuccess, nil
+    log.Infof("[directdns] no direct match, trying indirect for: %s", queryName)
+    return directdns.handleIndirectQuery(ctx, w, r)
 }
 
-func normalizeZone(zone string) string {
-	// dns://example.com.:53
-	if strings.HasPrefix(zone, "dns://") {
-		zone = strings.TrimPrefix(zone, "dns://")
-		if i := strings.Index(zone, ":"); i != -1 {
-			zone = zone[:i]
-		}
-	}
-	return dns.Fqdn(zone)
+func (directdns *DirectDNS) handleDirectQuery(ctx context.Context, r *dns.Msg, w dns.ResponseWriter, zone, queryName string) (int, error) {
+    log.Infof("[directdns] handleDirectQuery: query=%s, zone=%s", queryName, zone)
+    subdomain := trimZone(queryName, zone)
+    log.Infof("[directdns] subdomain: %q", subdomain)
+    if subdomain == "" {
+        return plugin.NextOrFailure(directdns.Name(), directdns.Next, ctx, w, r)
+    }
+    ipv6 := decodeIPv6(subdomain)
+    log.Infof("[directdns] decoded IPv6: %s", ipv6)
+    if ipv6 == "" {
+        return plugin.NextOrFailure(directdns.Name(), directdns.Next, ctx, w, r)
+    }
+
+    resp, err := forwardToNode(r, ipv6)
+    if err != nil || resp == nil {
+        log.Infof("[directdns] forward failed: %v, falling back", err)
+        return plugin.NextOrFailure(directdns.Name(), directdns.Next, ctx, w, r)
+    }
+
+    resp.SetReply(r)
+    resp.Authoritative = true
+    w.WriteMsg(resp)
+    return dns.RcodeSuccess, nil
 }
 
-func (directdns *DirectDNS) matchZone(query_name string) string {
-	for _, raw_zone := range directdns.Zones {
-		zone := normalizeZone(raw_zone)
-		if plugin.Name(zone).Matches(query_name) {
-			return dns.Fqdn(zone)
-		}
-	}
-	return ""
+func (directdns *DirectDNS) handleIndirectQuery(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+    log.Infof("[directdns] handleIndirectQuery called")
+    if directdns.Next == nil {
+        log.Infof("[directdns] Next is nil, returning")
+        return plugin.NextOrFailure(directdns.Name(), directdns.Next, ctx, w, r)
+    }
+
+    cw := &captureWriter{ResponseWriter: w}
+    rcode, err := directdns.Next.ServeDNS(ctx, cw, r)
+    if err != nil {
+        log.Infof("[directdns] Next returned error: %v", err)
+        return rcode, err
+    }
+    resp := cw.msg
+    if resp == nil {
+        log.Infof("[directdns] Next returned nil response")
+        return rcode, nil
+    }
+
+    log.Infof("[directdns] indirect response answer: %v", resp.Answer)
+    newAnswer, modified := directdns.rewriteCNAMEs(resp, r)
+    if modified {
+        log.Infof("[directdns] CNAME rewritten, new answer: %v", newAnswer)
+        newResp := new(dns.Msg)
+        newResp.SetReply(r)
+        newResp.Authoritative = resp.Authoritative
+        newResp.Answer = newAnswer
+        newResp.Ns = resp.Ns
+        newResp.Extra = resp.Extra
+        newResp.Rcode = resp.Rcode
+        w.WriteMsg(newResp)
+        return dns.RcodeSuccess, nil
+    }
+
+    log.Infof("[directdns] no CNAME rewrite, passing through")
+    w.WriteMsg(resp)
+    return rcode, nil
 }
 
-func trimZone(query_name, zone string) string {
-	query_name = dns.Fqdn(query_name)
-	zone = dns.Fqdn(zone)
-	if !strings.HasSuffix(query_name, zone) {
-		return ""
-	}
-	return strings.TrimSuffix(query_name, zone)
+func (directdns *DirectDNS) rewriteCNAMEs(originalResp *dns.Msg, r *dns.Msg) ([]dns.RR, bool) {
+    var newAnswer []dns.RR
+    found := false
+
+    for i, rr := range originalResp.Answer {
+        if found {
+            break
+        }
+        newAnswer = append(newAnswer, rr)
+        cname, ok := rr.(*dns.CNAME)
+        if !ok {
+            continue
+        }
+
+        log.Infof("[directdns] checking CNAME target: %s", cname.Target)
+        targetZone := directdns.matchZone(cname.Target)
+        if targetZone == "" {
+            log.Infof("[directdns] CNAME target not in our zones")
+            continue
+        }
+
+        subdomain := trimZone(cname.Target, targetZone)
+        ipv6 := decodeIPv6(subdomain)
+        if ipv6 == "" {
+            log.Infof("[directdns] could not decode IPv6 from subdomain: %s", subdomain)
+            continue
+        }
+
+        log.Infof("[directdns] querying IPv6 %s for CNAME target %s", ipv6, cname.Target)
+        ipv6Resp, err := forwardToNodeWithName(r, ipv6, cname.Target)
+        if err != nil || ipv6Resp == nil {
+            log.Infof("[directdns] IPv6 query failed: %v", err)
+            continue
+        }
+
+        found = true
+        newAnswer = newAnswer[:i+1]
+        newAnswer = append(newAnswer, ipv6Resp.Answer...)
+    }
+
+    return newAnswer, found
+}
+
+func (directdns *DirectDNS) matchZone(queryName string) string {
+    for _, zone := range directdns.Zones {
+        if plugin.Name(zone).Matches(queryName) {
+            return zone
+        }
+    }
+    return ""
+}
+
+func trimZone(queryName, zone string) string {
+    queryName = dns.Fqdn(queryName)
+    zone = dns.Fqdn(zone)
+    if !strings.HasSuffix(queryName, zone) {
+        return ""
+    }
+    return strings.TrimSuffix(queryName, zone)
 }
 
 func lastLabel(name string) string {
-	name = strings.TrimSuffix(name, ".")
-	parts := strings.Split(name, ".")
-	if len(parts) == 0 {
-		return ""
-	}
-	return parts[len(parts)-1]
+    name = strings.TrimSuffix(name, ".")
+    parts := strings.Split(name, ".")
+    if len(parts) == 0 {
+        return ""
+    }
+    return parts[len(parts)-1]
 }
 
 func decodeIPv6(subdomain string) string {
-	label := lastLabel(subdomain)
-	ip := strings.ReplaceAll(label, "-", ":")
-	if net.ParseIP(ip) == nil {
-		return ""
-	}
-	return ip
+    label := lastLabel(subdomain)
+    ip := strings.ReplaceAll(label, "-", ":")
+    if net.ParseIP(ip) == nil {
+        return ""
+    }
+    return ip
 }
 
 func forwardToNode(req *dns.Msg, ipv6 string) (*dns.Msg, error) {
-	c := &dns.Client{
-		Net:     "udp",
-		Timeout: 2 * time.Second,
-	}
+    c := &dns.Client{
+        Net:     "udp",
+        Timeout: 2 * time.Second,
+    }
 
-	addr := "[" + ipv6 + "]:53"
-	resp, _, err := c.Exchange(req, addr)
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
+    addr := "[" + ipv6 + "]:53"
+    resp, _, err := c.Exchange(req, addr)
+    return resp, err
+}
+
+func forwardToNodeWithName(req *dns.Msg, ipv6 string, name string) (*dns.Msg, error) {
+    newReq := req.Copy()
+    if len(newReq.Question) > 0 {
+        newReq.Question[0].Name = dns.Fqdn(name)
+    }
+
+    c := &dns.Client{
+        Net:     "udp",
+        Timeout: 2 * time.Second,
+    }
+
+    addr := "[" + ipv6 + "]:53"
+    resp, _, err := c.Exchange(newReq, addr)
+    return resp, err
 }
--- setup.go
+++ setup.go
@@ -4,18 +4,31 @@ import (
     "github.com/coredns/caddy"
     "github.com/coredns/coredns/core/dnsserver"
     "github.com/coredns/coredns/plugin"
+    "github.com/coredns/coredns/plugin/pkg/log"
+    "github.com/miekg/dns"
 )
 
 func init() { plugin.Register("directdns", setup) }
 
 func setup(c *caddy.Controller) error {
-    dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
-        return &DirectDNS{
-            Next:  next,
-            Zones: c.ServerBlockKeys,
+    d := &DirectDNS{}
+    for c.Next() {
+        args := c.RemainingArgs()
+        if len(args) == 0 {
+            return c.ArgErr()
         }
+        for _, zone := range args {
+            normalized := dns.Fqdn(zone)
+            log.Infof("[directdns] adding zone: %s (from %s)", normalized, zone)
+            d.Zones = append(d.Zones, normalized)
+        }
+    }
+
+    dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
+        d.Next = next
+        log.Infof("[directdns] plugin instance created, Next is nil: %v", next == nil)
+        return d
     })
 
     return nil
 }
-
